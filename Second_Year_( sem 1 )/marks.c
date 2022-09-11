d58fb8748685ff2ba91697fab4bcaa432563718f
Krishna Sen Program <98060735+Krishna-Sen-Programming-World@users.noreply.github.com>
2022-09-11 19:36:52 +0530

    Create marks.c

---
+++ Second_Year_( sem 1 )/marks.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int arr[5][3];
+    printf("Enter the student the marks:\n");
+    for(int i=0;i<5;i++){
+        for(int j=0;j<3;j++)
+        scanf("%d",&arr[i][j]);
+    }
+    printf("\nThe student the marks are\n");
+    for(int i=0;i<5;i++){
+        for(int j=0;j<3;j++)
+        printf("\t%d",arr[i][j]);
+        printf("\n");
+    }
+    for(int i=0;i<3;i++){
+        int max=arr[i][0];
+        int id=0;
+        for(int j=1;j<5;j++){
+            if(max<arr[j][i]){
+                max=arr[j][i];
+                id=j;
+            }
+        }
+        printf("\nindex[%d] student got max marks in subject no:%d which is %d",id,i,max);
+    }
+}
